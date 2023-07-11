681760a76747c38fcb4a5cfdbe77cff57269d75a
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-07-10 21:16:49 -0400

    Edits Html, starts event listener for form.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+const url = "https://pokeapi.co/api/v2/item"
+
+const form = document.querySelector("form")
+const div = document.querySelector("div")
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+})
